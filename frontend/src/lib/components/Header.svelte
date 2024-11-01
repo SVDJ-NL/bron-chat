392af476a0a9d0dc0681888186412ac4b4b55e5f
Linksmith <info@linksmith.nl>
2024-11-01 10:30:48 -0400

    Branding added

--- frontend/src/lib/components/Header.svelte
+++ frontend/src/lib/components/Header.svelte
@@ -6,8 +6,21 @@
     <div class="px-4 sm:px-6 lg:px-8">
         <div class="flex h-16">
             <div class="flex-shrink-0 flex items-center">
-                <img class="hidden sm:block h-8 w-auto" src="/bron-logo.svg" alt="Bron Logo" />
-                <img class="block sm:hidden h-6 w-auto" src="/bron-logo.svg" alt="Bron Logo" />
+                <a href="https://bron.live" target="_blank">
+                    <img class="hidden sm:block h-8 w-auto ml-1" src="/bron-logo.svg" alt="Bron Logo" />
+                    <img class="block sm:hidden h-6 w-auto ml-0.5" src="/bron-logo.svg" alt="Bron Logo" />
+                </a>
+                <span class="text-[#2260FF] text-2xl md:text-3xl font-semibold ml-2 uppercase mt-1 -ml-2 pl-2">Chat</span>
+            </div>
+            <div class="flex-shrink-0 flex items-center ml-auto">
+                <a href="https://svdj.nl" target="_blank">
+                    <img class="hidden sm:block h-7 md:h-12 w-auto" src="/incubator.png" alt="SvdJ Incubator Logo" />
+                    <img class="block sm:hidden h-6 md:h-10 w-auto" src="/incubator.png" alt="SvdJ Incubator Logo" />
+                </a>
+                <a href="https://openstate.eu" target="_blank">
+                    <img class="hidden sm:block h-7 md:h-11 w-auto" src="/open-state-foundation-logo.svg" alt="Open State Foundation Logo" />
+                    <img class="block sm:hidden h-6 md:h-9 w-auto" src="/open-state-foundation-logo.svg" alt="Open State Foundation Logo" />
+                </a>
             </div>
         </div>
     </div>
