3dd809929273a5495ceaf208184522321f24d1d9
Linksmith <info@linksmith.nl>
2024-11-05 09:45:09 -0500

    Fix broken uvicorn commands

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,5 +4,5 @@
 if [ "$ENVIRONMENT" = "development" ]; then
     exec uvicorn app.main:fast_api_app --host 0.0.0.0 --port 8000 --reload
 else
-    exec uvicorn app.main:fast_api_app --host 0.0.0.0 --port 8000 --workers 16 --timeout 300 --backlog 2048
+    exec uvicorn app.main:fast_api_app --host 0.0.0.0 --port 8000 --workers 16
 fi
