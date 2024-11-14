00d202c087c91ab0571b9edeb6f6cc0be27bb575
Linksmith <info@linksmith.nl>
2024-11-14 10:52:21 -0500

    header styling tweaks

--- frontend/src/lib/components/Header.svelte
+++ frontend/src/lib/components/Header.svelte
@@ -70,11 +70,11 @@
                     </svg> -->
                 </a>
                 <img
-                    class="h-4 sm:h-5 -mt-2 sm:-mt-1 ml-1"
+                    class="h-4 sm:h-5 -mt-2 sm:-mt-2 ml-1"
                     src="/beta-badge.svg" 
                     alt="Beta"
                 />
-                <a href="/" target="_blank" class="flex ml-2 font-medium text-sm sm:text-base items-center mt-1 sm:mt-2">                      
+                <a href="/" target="_blank" class="flex ml-2 font-medium text-sm sm:text-base items-center mt-1 sm:mt-2 hover:underline">                      
                     <span class="pr-1">Nieuwe chat</span>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3 sm:w-4 h-3 sm:h-4">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
