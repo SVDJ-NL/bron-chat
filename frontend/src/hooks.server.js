a4143847f1ca3a6b599256d4de50c8f350382175
Linksmith <info@linksmith.nl>
2024-12-02 21:40:44 +0100

    Possible fix for plausible code

--- frontend/src/hooks.server.js
+++ frontend/src/hooks.server.js
@@ -1,24 +1,17 @@
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
-    const response = await resolve(event);
-    
-    if (response.headers.get('content-type')?.includes('text/html')) {
-        let html = await response.text();
-        
-        if (process.env.NODE_ENV === 'production') {
-            const host = event.url.host;
-            
-            html = html.replace(
-                '</head>',
-                `<script defer data-domain="${host}" src="https://plausible.io/js/script.js"></script></head>`
-            );
+    const response = await resolve(event, {
+        transformPageChunk: ({ html }) => {
+            if (process.env.NODE_ENV === 'production') {
+                const host = event.url.host;
+                return html.replace(
+                    '</head>',
+                    `<script defer data-domain="${host}" src="https://plausible.io/js/script.js"></script></head>`
+                );
+            }
+            return html;
         }
-        
-        return new Response(html, {
-            status: response.status,
-            headers: response.headers
-        });
-    }
+    });
     
     return response;
 } 
